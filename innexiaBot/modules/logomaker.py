0a0f6ff74ee0b0cd3867769638d941ad0570eef0
dyno-create <82661019+dyno-create@users.noreply.github.com>
2021-10-20 09:28:39 +0530

    Update logomaker.py

--- innexiaBot/modules/logomaker.py
+++ innexiaBot/modules/logomaker.py
@@ -100,7 +100,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByInnexia.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="💕 Made By @SiderzBot Support @SiderzChat")
+    await tbot.send_file(event.chat_id, fname2, caption="Your Logo Sir")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -140,7 +140,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByInnexia.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @innexiaBot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made by Cobra Robot😈)
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
